877ee4753c785a0f8914fb4e5e7f61b2f7c2094c
YongBum <dt10002@naver.com>
2024-10-15 21:34:49 +0900

    init directory structure

---
+++ src/test/java/custom/exceptionex/ExceptionexApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.exceptionex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
